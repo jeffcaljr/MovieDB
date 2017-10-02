68973107aa5cfa3a635c5cd9856eda0467a27368
Jeffery Calhoun <jeffcaljr@gmail.com>
2017-10-02 15:04:24 -0500

    medium sized devices now show 3 movies per row instead of 6

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -26,7 +26,7 @@ class MovieItem extends Component{
 
     render(){
         return (
-            <div className="movie-item-container-single col-6 col-sm-2 col-md-4 col-lg-3 col-xl-2">
+            <div className="movie-item-container-single col-6 col-sm-3 col-md-4 col-lg-3 col-xl-2">
                 <figure
                     className="movie-item image-btn-container"
                     onClick={() => this.props.openDetailModal()}>
