2c4ba23479420d35d993316d991ea0533f41997d
Jeffery Calhoun <jeffcaljr@gmail.com>
2017-10-03 23:50:49 -0500

    attempting to fix issue where mobile nav sometimes doesnt close when category selected

--- src/reducers/MobileNav.js
+++ src/reducers/MobileNav.js
@@ -9,9 +9,7 @@ const MobileNavReducer = (state = { expanded: false}, action) =>{
         case CONTRACT:
             return Object.assign({}, state, {expanded: false})
         case MENU_ITEM_CHOSEN:
-            if(state.expanded){
-                action.asyncDispatch(contractNav())
-            }
+            action.asyncDispatch(contractNav())
             return state;
         default:
             return state
