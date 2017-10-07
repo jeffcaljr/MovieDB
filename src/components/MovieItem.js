0bbc53307dc457a3700803bb8d4b646d9c05a893
Jeffery Calhoun <jeffcaljr@gmail.com>
2017-10-06 19:23:00 -0500

    refactored scrolling text box into seperate component for reuse

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -7,52 +7,37 @@ import cover from '../images/default-movie-cover.jpg'
 import FavoriteButton from "./FavoriteButton";
 import {show} from "../actions/MovieDetailModal";
 import {error, toggleLiked} from "../actions/MovieList";
+import ScrollingTextView from "./ScrollingTextView";
 
 
 
-class MovieItem extends Component{
-    constructor(){
-        super()
-
-        this.hasOverflow= this.hasOverflow.bind(this)
-
-        this.state = {
-            hasOverflow: false
-        }
-    }
-
-    hasOverflow = () =>{
-        return this.scrollLeft.offsetWidth < this.scrollText.offsetWidth
-    }
-
-    render(){
-        return (
-            <div className="movie-item-container-single col-6 col-sm-3 col-md-4 col-lg-3 col-xl-2">
-                <figure
-                    className="movie-item image-btn-container w-100"
-                    onClick={() => this.props.showDetailModal()}>
-                    <img src={ this.props.movie.getImage() || cover} alt={this.props.movie.title || "Movie Cover"} className="movie-img"/>
-
-                    <div className="text-white text-center movie-item-details">
-                        <FavoriteButton isFavorite={this.props.isFavorite} toggledFavorite={() => this.props.toggleFavorite()}/>
-                        <div className={"scroll-left"}
-                             ref={(scrollLeft) => this.scrollLeft = scrollLeft}>
-                            <p className={"movie-item-details-title typeface-sans-serif" + (this.state.hasOverflow ? " scrolling-title w-100 " : " no-scrolling-title ")} ref={(scrollText) => this.scrollText = scrollText}>{this.props.movie.title}</p>
-
-                        </div>
-                    </div>
-
-                </figure>
+const MovieItem = ({movie, similarMovie, isFavorite, toggleFavorite, showDetailModal}) => {
 
 
-            </div>
-        );
-    }
+    return (
+        <div className="movie-item-container-single col-6 col-sm-3 col-md-4 col-lg-3 col-xl-2">
+            <figure
+                className="movie-item image-btn-container w-100"
+                onClick={() => showDetailModal()}>
+                <img src={ movie.getImage() || cover} alt={movie.title || "Movie Cover"} className="movie-img"/>
 
-    componentDidMount(){
+                <div className="text-white text-center movie-item-details">
+                    <FavoriteButton isFavorite={isFavorite} toggledFavorite={() => toggleFavorite()}/>
 
-        this.setState({ hasOverflow: this.hasOverflow()})
-    }
+                    <ScrollingTextView
+                        className="movie-item-details-title typeface-sans-serif"
+                        text={movie.title}
+                    />
+                </div>
+
+
+
+            </figure>
+
+
+        </div>
+    );
+
 
 
 }
---
+++ src/components/ScrollingTextView.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class ScrollingTextView extends Component{
+    constructor(){
+        super()
+
+        this.state = {
+            hasOverflow: false
+        }
+
+        this.hasOverflow= this.hasOverflow.bind(this)
+
+    }
+
+    hasOverflow = () =>{
+        return this.scrollLeft.offsetWidth < this.scrollText.offsetWidth
+    }
+
+    render(){
+        return(
+
+            <div className="scroll-left"
+                 ref={(scrollLeft) => this.scrollLeft = scrollLeft}>
+                <p
+                    className={this.props.className + " " + (this.state.hasOverflow ? " scrolling-title w-100 " : " no-scrolling-title ")}
+                    ref={(scrollText) => this.scrollText = scrollText}>
+
+                    {this.props.text}
+                </p>
+
+            </div>
+
+        );
+    }
+
+    componentDidMount(){
+
+        this.setState({ hasOverflow: this.hasOverflow()})
+    }
+}
+
+
+ScrollingTextView.propTypes = {
+    className: PropTypes.string,
+    text: PropTypes.string.isRequired
+
+}
+
+export default ScrollingTextView;
